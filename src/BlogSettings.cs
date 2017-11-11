8b30527579ba2fc78c338dc67d029b9edeaa6de7
Aivaras <aivaras.simulis@outlook.com>
2017-11-11 19:57:59 +0200

    Initialize

--- src/BlogSettings.cs
+++ src/BlogSettings.cs
@@ -2,10 +2,10 @@
 {
     public class BlogSettings
     {
-        public string Name { get; set; } = "Miniblog.Core";
-        public string Description { get; set; } = "A short description of the blog";
-        public string Owner { get; set; } = "The Owner";
-        public int PostsPerPage { get; set; } = 2;
-        public int CommentsCloseAfterDays { get; set; } = 10;
+        public string Name { get; set; } = "Aivaras Šimulis";
+        public string Description { get; set; } = "A simplistic approach to web development";
+        public string Owner { get; set; } = "Aivaras Šimulis";
+        public int PostsPerPage { get; set; } = 5;
+        public int CommentsCloseAfterDays { get; set; } = 14;
     }
 }
